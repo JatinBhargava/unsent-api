53f0a20ffcf7c01da3bc7b16aea0f35f4c1c159a
Jatin <jatin@Jatins-MacBook-Air.local>
2026-01-25 15:20:31 +0530

    starter for api repository

---
+++ src/main/java/com/unsent/api/UnsentApiApplication.java
@@ -0,0 +1,13 @@
+package com.unsent.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsentApiApplication.class, args);
+	}
+
+}
