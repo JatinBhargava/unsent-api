4174b5c85ac6bcfd865157c10cf9a370a07d4c7e
jatin <jatinbhargava4@gmail.com>
2026-02-07 18:40:00 +0530

    listener and batch application setup

--- unsent-listener/src/main/java/com/unsent/unsent_listener/UnsentListenerApplication.java
+++ unsent-listener/src/main/java/com/unsent/unsent_listener/UnsentListenerApplication.java
@@ -2,7 +2,9 @@ package com.unsent.unsent_listener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class UnsentListenerApplication {
 
