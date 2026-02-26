72fdc745c340a3d30fde6d8d5a858fa513479b92
jatin <jatinbhargava4@gmail.com>
2026-02-27 00:06:44 +0530

    "added Oauth and linux/arm64 image upgrade"

--- unsent-api/src/main/java/com/unsent/api/config/SecurityConfig.java
+++ unsent-api/src/main/java/com/unsent/api/config/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.unsent.api.config;
 
+import com.unsent.api.Util.OAuthSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    OAuthSuccessHandler authSuccessHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .successHandler(authSuccessHandler));
         return http.build();
     }
 }
--- unsent-api/src/main/java/com/unsent/api/service/UserService.java
+++ unsent-api/src/main/java/com/unsent/api/service/UserService.java
@@ -2,6 +2,7 @@ package com.unsent.api.service;
 
 import com.unsent.api.entity.User;
 import com.unsent.api.repository.UserRepository;
+import com.unsent.util.Gender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -24,5 +25,23 @@ public class UserService {
 
     public Optional<User> findByEmail(final String email){
         return userRepository.findByEmail(email);
+    }
+
+    public User findOrCreate(String email) {
+        return userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    User user = new User();
+                    user.setEmail(email);
+                    user.setUsername(generateUsername(email));
+                    user.setDisplayName("Anonymous");
+                    user.setGender(Gender.PREFER_NOT_TO_SAY.getCode());
+                    user.setDateOfBirth(null);
+                    user.setHashedPassword(null); // OAuth users don’t need password
+                    return userRepository.save(user);
+                });
+    }
+
+    private String generateUsername(String email) {
+        return email.split("@")[0] + "_" + System.currentTimeMillis();
     }
 }
